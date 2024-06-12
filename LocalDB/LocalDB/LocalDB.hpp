41dd5ee8ec5a6ce47ad8d2358f8599346afd5f4b
Mihir Aryan <mihir.aryan@tallysolutions.com>
2024-06-12 11:11:43 +0530

    Added UpdateTableData function
     - This function can be used to update the specific row data

--- LocalDB/LocalDB/LocalDB.hpp
+++ LocalDB/LocalDB/LocalDB.hpp
@@ -12,7 +12,7 @@ using namespace sql;
 #define TABLE_ALTERED                          "Table Altered Successfully"
 #define DATA_INSERTED                          "Data Inserted Successfully"
 #define ALL_DATA_DELETED                       "All Data Delete Successfully"
-#define SHOW_TABLE_COLUMN_DATA                 "Shown Table Column Data Successfully"
+#define TABLE_COLUMN_DATA_UPDATED              "Table Column Data is Successfully Updated"
 
 #define INSUFFICIENT_PARAMS_CREATEDB           "Insufficient TDL Parameters. Expected parameters are '<Server>:<User Name>:<Password>:<DataBase Name>'"
 #define INSUFFICIENT_PARAMS_CREATE_TABLE       "Insufficient TDL Parameters. Expected parameters for CreateTable are '<Server>:<User Name>:<Password>:<DataBase Name>:<Table Name>:<Column Name>:<Column Data Type>'"
@@ -20,7 +20,9 @@ using namespace sql;
 #define INSUFFICIENT_PARAMS_ALTER_TABLE        "Insufficient TDL Parameters. Expected parameters for AlterTable are '<Server>:<User Name>:<Password>:<DataBase Name>:<Table Name>:<Column Name>:<Column Data Type>'"
 #define INSUFFICIENT_PARAMS_INSERT_DATA        "Insufficient TDL Parameters. Expected parameters for InsertData are '<Server>:<User Name>:<Password>:<DataBase Name>:<Table Name>':<Column Name>:<Column Value>"
 #define INSUFFICIENT_PARAMS_DELETE_ALL_DATA    "Insufficient TDL Parameters. Expected parameters for DeleteAllData are '<Server>:<User Name>:<Password>:<DataBase Name>:<Table Name>'"
-#define INSUFFICIENT_PARAMS_SHOW_DATA          "Insufficient TDL Parameters. Expected parameters for DeleteAllData are '<Server>:<User Name>:<Password>:<DataBase Name>:<Table Name>:<Column Name 1>:<Column Name 2>'"
+#define INSUFFICIENT_PARAMS_SHOW_DATA          "Insufficient TDL Parameters. Expected parameters for ShowTableData are '<Server>:<User Name>:<Password>:<DataBase Name>:<Table Name>:<Column Name 1(Optional)>:<Column Name 2(Optional)>'"
+#define INSUFFICIENT_PARAMS_UPDATE_COLUMN_DATA "Insufficient TDL Parameters. Expected parameters for UpdateTableData are '<Server>:<User Name>:<Password>:<DataBase Name>:<Table Name>:<Column Name With Values>:<Condition>'"
+
 
 #define ALLOC_TLocalDataBase      new TLocalDataBase
 
@@ -38,16 +40,26 @@ inline              ~TLocalDataBase            ();
         eGoodBad    InsertData                 (Word pArgc, WStrPtr* pArgv);
         eGoodBad    DeleteAllData              (Word pArgc, WStrPtr* pArgv);
         eGoodBad    ShowTableData              (Word pArgc, WStrPtr* pArgv);
-
+        eGoodBad    UpdateTableData            (Word pArgc, WStrPtr* pArgv);
     private:
 
         eGoodBad    DataBase                   (AStrPtr pServer, AStrPtr pUserName, AStrPtr pPassword, AStrPtr pDataBaseName, eBool pCreateDb, StrPtr& pBadResponse);
-        eGoodBad    CreateLocalTable           (AStrPtr pServer, AStrPtr pUserName, AStrPtr pPassword, AStrPtr pDataBaseName, AStrPtr pTableName, AStrPtr pColumnName, AStrPtr pColumnDataType, StrPtr& pBadResponse);
-        eGoodBad    AlterLocalTable            (AStrPtr pServer, AStrPtr pUserName, AStrPtr pPassword, AStrPtr pDataBaseName, AStrPtr pTableName, AStrPtr pColumnName, AStrPtr pColumnDataType, StrPtr& pBadResponse);
+        eGoodBad    CreateLocalTable           (AStrPtr pServer, AStrPtr pUserName, AStrPtr pPassword, AStrPtr pDataBaseName, AStrPtr pTableName, AStrPtr pColumnName, 
+                                                AStrPtr pColumnDataType, StrPtr& pBadResponse);
+
+        eGoodBad    AlterLocalTable            (AStrPtr pServer, AStrPtr pUserName, AStrPtr pPassword, AStrPtr pDataBaseName, AStrPtr pTableName, AStrPtr pColumnName, 
+                                                AStrPtr pColumnDataType, StrPtr& pBadResponse);
+
         eGoodBad    DeleteLocalTable           (AStrPtr pServer, AStrPtr pUserName, AStrPtr pPassword, AStrPtr pDataBaseName, AStrPtr pTableName, StrPtr& pBadResponse);
-        eGoodBad    InsertDataIntoTable        (AStrPtr pServer, AStrPtr pUserName, AStrPtr pPassword, AStrPtr pDataBaseName, AStrPtr pTableName, AStrPtr pColumnNames, AStrPtr pValues, StrPtr& pBadResponse);
+        eGoodBad    InsertDataIntoTable        (AStrPtr pServer, AStrPtr pUserName, AStrPtr pPassword, AStrPtr pDataBaseName, AStrPtr pTableName, AStrPtr pColumnNames, 
+                                                AStrPtr pValues, StrPtr& pBadResponse);
+
         eGoodBad    DeleteAllDataFromTable     (AStrPtr pServer, AStrPtr pUserName, AStrPtr pPassword, AStrPtr pDataBaseName, AStrPtr pTableName, StrPtr& pBadResponse);
-        eGoodBad    ShowTableColumnData        (Word pArgc, AStrPtr pServer, AStrPtr pUserName, AStrPtr pPassword, AStrPtr pDataBaseName, AStrPtr pTableName, AStrPtr pColumnName1, AStrPtr pColumnName2, StrPtr& pResult, StrPtr& pBadResponse);
+        eGoodBad    ShowTableColumnData        (Word pArgc, AStrPtr pServer, AStrPtr pUserName, AStrPtr pPassword, AStrPtr pDataBaseName, AStrPtr pTableName, AStrPtr pColumnName1, 
+                                                AStrPtr pColumnName2, StrPtr& pResult, StrPtr& pBadResponse);
+
+        eGoodBad    UpdateColumnData           (AStrPtr pServer, AStrPtr pUserName, AStrPtr pPassword, AStrPtr pDataBaseName, AStrPtr pTableName, AStrPtr pColumnDataValue, 
+                                                AStrPtr pConditionData, StrPtr& pBadResponse);
 
         void        SetResult                  (CWStrPtr pVal);
         void        SetResult                  (CAStrPtr pVal);
